5e9010c0e21f8e5b3b22aae6ab3601654f0ba0b4
sona <sennansona@gmail.com>
2024-05-22 15:19:35 +0530

    Add print statements with comments
    Added print statements to display strings and arithmetic operations, each followed by comments providing explanations.

---
+++ ex3.py
@@ -0,0 +1,25 @@
+print("I will now count my chickens")         #prints a strings which states that the chickens will be counted.
+
+print("Hens",25+30/6)                         #prints the name 'Hens' followed by the arithmatic operation (25+30/6).      
+print("Roosters",100-25*3%4)                  #prints the name 'Roosters' followed by the arithmatic operation (100-25*3%4).
+
+print("Now i will count the eggs:")           #prints a strings which indicates that the egg counting will be started now.
+
+print(3+2+1-5+4%2-1/4+6)                      #Runs and prints the arithmatic operation. 
+
+print("Is it true that 3+2<5-7?")             #Prints a string with a question.
+
+print(3+2<5-7)                                #Runs and prints the arithmatic comparison.
+
+print("What is 3+2?",3+2)                     #Prints a string asking a mathematical question followed by it's result.
+print("What is 5-7?",5-7)
+
+print("Oh,that's why it's false")             #Prints a string indicating the understanding of above code lines.
+
+print("How about some more.")                 #string askng more information            
+
+print("Is it greater?",5>-2)                  #Prints a string asking a comparison followed by it's result.
+print("Is it greater or equal?",5>=-2)        #Prints a string asking a comparison followed by it's result.      
+print("Is it less or equal",5<=-2)            #Prints a string asking a comparison followed by it's result.    
+
+
