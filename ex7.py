2872e30b73c21b15f33841f9c426f45dfb2f00c5
sona <sennansona@gmail.com>
2024-05-22 21:49:29 +0530

    Implemented code to print and concatenate strings.

---
+++ ex7.py
@@ -0,0 +1,21 @@
+print("Mary had a little lamb.") #To print the string'Mary had a little lamb'.
+print("Its fleece was white as {}.".format('snow'))#prints 'Its fleece was white as snow'.
+print("And everywhere that Mary went.") #To print the string'And everywhere that Mary went.'.
+print("." * 10)#To print '.'  10 times.
+
+end1 = "C" #Assigning a single character to the variable.  
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end.try removing it to see what happens 
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ') #concatenation of the variables from edn1 to end6.
+print(end7 + end8 + end9 + end11 + end12)  #concatenation of the variables from edn7 to end12.
