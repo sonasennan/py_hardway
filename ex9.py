7a3f8dc649482f823020550067b6baa21b4235ce
sona <sennansona@gmail.com>
2024-05-23 12:08:54 +0530

    Added code with multi-line printing.

---
+++ ex9.py
@@ -0,0 +1,14 @@
+#Here's some new strange stuff,remember type it exactly.
+
+days="Mon Tue Wed Thu Fri Sat Sun"        #initializing a variable named 'days' with values.
+months="\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"     #initializing a variable named 'months' with values.
+
+print("Here are the days:",days)      #accessing the value of 'date' along with a print message.
+print("Here are the months:",months)  #accessing the value of 'month' along with a print message.
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as mucch as we like.
+Even 4 lines if we want,or 5,or 6.               
+""")                                         #shows an example of using three double quotes.
