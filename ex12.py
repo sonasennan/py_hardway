f8defc67ee4987939afa76eab4c61dd091156e82
sona <sennansona@gmail.com>
2024-05-24 09:55:30 +0530

    Added a python program to accept input from user and display it using string formatting.

---
+++ ex12.py
@@ -0,0 +1,5 @@
+age=input("How old are you? ")   #takes input from the user and assign it to the variable "age".
+height=input("How tall are you? ")    #takes input from the user and assign it to the variable "height".
+weight=input("How much do you weigh? ")     #takes input from the user and assign it to the variable "weight".
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")  #used formatting and accesed the values from variables.
