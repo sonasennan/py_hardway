b97a7276220ca88da03d197e2692802a2bbf55ca
sona <sennansona@gmail.com>
2024-05-24 10:37:30 +0530

    Implemented a program using argv module in sys library and used unpacking of command-line.

---
+++ ex13.py
@@ -0,0 +1,6 @@
+from sys import argv  #adding feature to your script from python feature set.
+script, first, second, third = argv    #unpacking the commands in arguments and passed them to variables.
+print("The script is called:", script)   #prints the name of the script,ie the name of your python file.
+print("your first variable is:", first)   #accesing and displaying the value of first command-line argument.
+print("your second variable is:", second)
+print("your third variable is:", third)
