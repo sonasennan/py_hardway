d35f75c46ad9e3f65376067a002ae11045a40257
sona <sennansona@gmail.com>
2024-05-30 23:03:51 +0530

    Implemented code to get familiarize with logical operators if-statement.

---
+++ ex29.py
@@ -0,0 +1,29 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:                  #check whether the no: of people is less than the no: of cats.
+    print("Too many cats! The world is doomed!")     #this will be printed only if the condition evaluates to true.
+
+if people > cats:   #less than.
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:
+    print("The world is dry!")
+
+
+dogs += 5    #increamenting the value of dogs by 5.
+
+if people >= dogs:   #greater than or equal to.
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs:  #less than or equal to.
+    print("People are less than or equal to dogs.")
+
+
+if people == dogs:  #equality operator.
+    print("People are dogs.")
